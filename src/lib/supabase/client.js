3b0c5dfc3223504bd4b7480b09feb17eb62851ef
Achal Singhal <achals2002@gmail.com>
2023-08-25 17:02:30 +0530

    hehe prt 2

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,20 +1,10 @@
-import { SUPABASE_SERVICE_ACCOUNT } from '$env/static/private';
 import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-	PUBLIC_SUPABASE_URL,
-	PUBLIC_SUPABASE_KEY,
-	{
-		auth: {
-			autoRefreshToken: true,
-			persistSession: false
-		}
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
+	auth: {
+		autoRefreshToken: true,
+		persistSession: false
 	}
-);
-
-export const supabaseAdmin = createClient(
-	PUBLIC_SUPABASE_URL,
-	SUPABASE_SERVICE_ACCOUNT
-);
+});
