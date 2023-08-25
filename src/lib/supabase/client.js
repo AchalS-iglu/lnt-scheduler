dc8a4be7e37154e0b050c2e848ef7a7604ffe6a4
Achal Singhal <achals2002@gmail.com>
2023-08-25 16:45:04 +0530

    env

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,9 +1,11 @@
+import { SUPABASE_SERVICE_ACCOUNT } from '$env/static/private';
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-	'https://tyewgcygpmalauumkzwx.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5ZXdnY3lncG1hbGF1dW1rend4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2ODUwODUsImV4cCI6MjAwODI2MTA4NX0.EnGFNkerx63OpWB1im0i-aUK10bB223BcbzTNKn_9hM',
+	PUBLIC_SUPABASE_URL,
+	PUBLIC_SUPABASE_KEY
 	{
 		auth: {
 			autoRefreshToken: true,
@@ -13,6 +15,6 @@ export const supabase = createClient(
 );
 
 export const supabaseAdmin = createClient(
-	'https://tyewgcygpmalauumkzwx.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5ZXdnY3lncG1hbGF1dW1rend4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjY4NTA4NSwiZXhwIjoyMDA4MjYxMDg1fQ.hxkBHNiGgS6bGhHhFiHvOS8B_VSdV0VKzIN3u98VgA4'
+	PUBLIC_SUPABASE_URL,
+	SUPABASE_SERVICE_ACCOUNT
 );
